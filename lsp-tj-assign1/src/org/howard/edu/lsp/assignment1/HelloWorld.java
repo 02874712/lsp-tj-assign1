f28a8c51ccf1ca5caa07f4a952e28e63f20d1145
02874712 <70228981+02874712@users.noreply.github.com>
2021-01-21 21:12:31 -0500

    Committing "Hello World"

---
+++ lsp-tj-assign1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+		
+		//Tashiana Johnson, Student Id: @02874712
+	}
+
+}
