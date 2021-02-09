955a5fbebc133b3c500d073fa7ef661d31b14804
02874712 <70228981+02874712@users.noreply.github.com>
2021-02-08 23:34:17 -0500

    Adding in source code-assignment 2(w/o comments)

--- lsp-tj-assign1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ lsp-tj-assign1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,12 +1,12 @@
-package org.howard.edu.lsp.assignment1;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		
-		//Tashiana Johnson, Student Id: @02874712
-	}
-
-}
+//package org.howard.edu.lsp.assignment1;
+//
+//public class HelloWorld {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("Hello World!");
+//		
+//		
+//	}
+//
+//}
